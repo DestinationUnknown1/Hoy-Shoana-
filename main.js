7f024506396587ff27347a9a8abd3f1e0d411742
DestinationUnknown1 <156078514+DestinationUnknown1@users.noreply.github.com>
2024-01-09 13:49:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,309 @@
+const QUESTIONS = [
+	{
+		question: "Д1ачакхдаккха кица:\"Дукха 1ийха циско...\"",
+		answers: ["Шур мена яц", "Дег1а даьккхадац", "Топ теха яц", "Дахка лаьцабац"],
+		correct: 4,
+	},
+
+	{
+		question: "\"Боадонгара\"ях роман хьано язъяй",
+		answers: [
+			"Яндиев Джамалд",
+			"Гагиев Гирихан",
+			"Базоркин Идрис",
+			"Мальсагов Дошлакъ",
+		],
+		correct: 3,
+	},
+	{
+		question: "\"Кит\" ях деша таржам де",
+		answers: [
+			"Сувсакх",
+			"Борцакх",
+			"Эпп",
+			"Болчакх",
+		],
+		correct: 1,
+	},
+	{
+		question: "Д1ачакхдаккха кица:\"Дайна сий...\"",
+		answers: ["Корадаьдац", "Меттаденадац", "Дайна ираз", "Ураг1дахадац"],
+		correct: 2,
+	},
+
+	{
+		question: "Ц1ерага маьре яхар \"-ях произведени язъяр малув",
+		answers: ["Чахкиев Саид", "Полонкоев Мухьаммад", "Боков Ахьмад", "Чахкиев Капитон"],
+		correct: 1,
+	},
+
+	{
+		question: "Носорог \"-ях деша таржам де",
+		answers: ["Ц1алкх", "Гамаж", "Бирнал", "Т1оаргац"],
+		correct: 3,
+	},
+
+	{
+	
+		question: "Г1алг1ай къам сибарег1а малаг1ча шера 1одахийтад?",
+		answers: ["1948 шера", "1956 шера", "1939 шера", "1944 шера"],
+		correct: 4,
+	},
+
+		{
+		question: "Г1алг1ай паччахьалкхе малаг1ча шера хьакхеллай?",
+		answers: ["1987 шера", "1995 шера", "1992 шера ", "1887 шера"],
+		correct: 3 ,
+	},
+
+		{
+		question: "\"Мулл\"-ях деша таржам де ",
+		answers: ["Вирб1арз", "Къайг", "Наж", "Нач1ал"],
+		correct: 1 ,
+	},
+
+	{
+		question: "Д1ачакхдаккха кица:\"Т1емо во1 ваьвац...\" ",
+		answers: ["Т1емо пха лийхаб", "Т1емо во1 вийнув", "Къаракха пайд хиннабац", "Т1емо во1 воаваьв"],
+		correct: 2,
+	},
+
+	{
+		question: "Г1алг1ай Республика хьалхара президент мал хиннав:",
+		answers: ["Аушев Руслан", "Мальсагов Ахьмад", "Евкуров Юнус-Бек", "Хашагульгов Бахьаудин"],
+		correct: 1,
+	},
+
+   {
+		question: "Г1алг1ай къамо им маца диллад:",
+		answers: ["1887 шера", "1847 шера", "1700 шера", "1930 шера"],
+		correct: 2,
+	},
+
+	{
+		question: "Пригородни районера г1алг1ай  малаг1ча шера арабаьхаб:",
+		answers: ["1920 шера", "1890 шера", "1970 шера", "1992 шера"],
+		correct: 4,
+	},
+
+    {
+		question: "Г1алг1ай алаппат хьано кхелад:",
+		answers: ["Мальсагов Зоврбик", "Боков Ахьмад", "Г1оандлой Хьасан", "Гомкартиев Сараждин"],
+		correct: 1,
+	},
+
+	{
+		question: "Г1алг1ай алаппат  масса алапаг1 латт:",
+		answers: ["33 алап", "26 алап", "46 алап", "40 алап"],
+		correct: 3,
+	},
+
+	{
+		question: "Г1алг1ай мехка эггара лакхаг1а лоам малаг1а ба:",
+		answers: ["Маьтт-лоам", "Шан-лоам ", "Цей-лоам", "Баш-лоам"],
+		correct: 2,
+	},
+
+	{
+		question: "Г1алг1ай мехка керттера г1ала  я:",
+		answers: ["Назрань", "Эбарг-Юрт", "Маг1албик", "Магас"],
+		correct: 4,
+	},
+
+	{
+		question: "Г1алг1ай эггара хьалхара элгац малаг1а хиннай:",
+		answers: ["Маьт-Сели", "Т1ой-Ерда", "Ткъоаба-Ерда", "Ийс Г1ала"],
+		correct: 3,
+	},
+
+	{
+		question: "Г1алг1ай байракха т1а латтар фуй? :",
+		answers: ["Тхьамаг1а", "Зялмаха", "Солярный-Хьарак", "Толмач"],
+		correct: 1,
+	},
+
+   {
+		question: "\"Бульон\"-яха деша таржам де :",
+		answers: ["Хьонк", "Ч1ондарг", "Дилл", "П1енд"],
+		correct: 3,
+	},
+
+	{
+		question: "Мехк-кхеле юкъе масса саг водаш хиннав:",
+		answers: ["22 саг", "12 саг", "21 саг", "19 саг"],
+		correct: 2,
+	},
+
+	{
+		question: "Г1алг1ай масса аьла хиннав:",
+		answers: ["3", "6", "9", "В1ала хиннавац"],
+		correct: 4,
+	},
+
+	{
+		question: "К1илбаседа-Кавказе эггара хьалхаг1а \"Дунен яхьаш\" котале яккхар малув :",
+		answers: ["Садулаев Абдул-Рашид", "Котиев Ахьмад", "Мальсагов Хьусен", "Кураев Сергей"],
+		correct: 2,
+	},
+
+
+	{
+		question: "Г1алг1аша шоалаг1а бувца мотт малаг1а ба:",
+		answers: ["Черсий мотт", "Г1умкий мотт", "Эрсий мотт", "Гуржий мотт"],
+		correct: 3,
+	},
+
+
+	{
+		question: "Д1ачакхдаккха кица: \"Лакха лоам хоарцабаьбац...\" ",
+		answers: ["Бакъдола дош а къардаьдац", "Нийс дош тулл даьдац", "Лакха гаьн хоадаяъяц", "Бошабаьб"],
+		correct: 1,
+	},
+
+	{
+		question: "Г1алг1ай къам Россе пачахьалкхен юкъе малаг1ча шера дахад",
+		answers: ["1730 шера", "1770 шера", "1880 шера", "1856 шера"],
+		correct: 2,
+	},
+
+	{
+		question: "Г1алг1ай метта масса падеж да:?",
+		answers: ["Кхоъ", "Ворх", "Пхиъ", "Барх"],
+		correct: 4,
+	},
+
+	{
+		question: "Малаг1ча дешагара хьахиннад \"Г1алг1айче\" яха дош ",
+		answers: ["Г1ала", "Г1алг1а", "Г1алг1ай", "Кхы долча дешаг1"],
+		correct: 1,
+	},
+
+	{
+		question: "Хьано язъяй \"Наьна илли\" яха байт ",
+		answers: ["Боков Ахьмад", "Яндиев Джамалд", "Хашагульгов 1аьла", "Осмиев Хьамзат"],
+		correct: 2,
+	},
+]
+
+
+
+const shuffle = (arr) => arr.sort(()=>0.5 - Math.random());
+shuffle(QUESTIONS);
+
+console.log(QUESTIONS)
+
+QUESTIONS.length = 15; 
+
+
+
+const headerContainer = document.querySelector('#header'),
+      listContainer   = document.querySelector('#list'),
+      submitBtn       = document.querySelector('.answbutton'),
+      counter         = document.querySelector('.count')
+      lovzar 		  = document.querySelector('.as')
+      photo			  = document.querySelector('.photo')
+      kicash 		  = document.querySelector('.about')
+      photo1		  = document.querySelector('.photo1')
+      duvcarash       = document.querySelector('.call')
+      photo2		  = document.querySelector('.photo2')
+
+
+
+
+
+
+
+
+let score = 0,
+    questionIndex = 0
+
+clearPage()
+showQuestion()
+submitBtn.onclick = checkAnswer;
+count()
+
+
+
+
+
+function count(){
+	let c = '%answ%/%total%'
+
+	let abc = c.replace('%answ%', questionIndex + 1 ).replace('%total%', QUESTIONS.length)
+
+	counter.innerHTML = abc
+
+}
+
+
+function clearPage() {
+	headerContainer.innerHTML = '';
+	listContainer.innerHTML = ''
+}
+
+function showResult() {
+	const resultsTemplate = `
+		<h2 class="title">Ваьлар хьо</br>
+		Нийс жоп денна хаттараш: %result%
+		</h2> 
+	`
+	
+
+	const results = resultsTemplate.replace('%result%', score)
+	headerContainer.innerHTML = results;
+
+}
+
+function showQuestion(){
+
+	const headerTemplate = `<h2 class="title">%template%</h2>`
+	const title = headerTemplate.replace('%template%', QUESTIONS[questionIndex]['question'])
+	headerContainer.innerHTML = title
+
+	for([index, item] of QUESTIONS[questionIndex ]['answers'].entries()){
+
+		const questionTemplate = `
+			<li>
+				<label>
+					<input value='%num%' type="radio" class="answer" name="answer">
+					<span>%answer%</span>
+				</label>
+			</li>
+		`
+
+		const answerQeus = questionTemplate.replace('%answer%', item).replace('%num%', index + 1)
+
+		listContainer.innerHTML += answerQeus
+	}
+}
+
+function checkAnswer() {
+
+	const checkedReadio = listContainer.querySelector('input[type = "radio"]:checked')
+
+	if (!checkedReadio) {
+				return
+	}
+
+	const userAnswer = +checkedReadio.value
+
+	if (userAnswer === QUESTIONS[questionIndex]['correct']) {
+		score++
+	}
+
+	if (questionIndex !== QUESTIONS.length - 1) {
+
+		questionIndex++
+		clearPage()
+		showQuestion()
+
+	} else {
+		clearPage()
+		showResult()
+	}
+
+	count()
+	submitBtn.blur();
+}
+
+
